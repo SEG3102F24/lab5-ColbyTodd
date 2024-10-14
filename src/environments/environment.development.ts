56e3d8ed58a2e6306fb0b8185f5445c960426e9d
Colby Todd <cjhtodd11@gmail.com>
2024-10-14 17:52:21 -0400

    Added firebase connection

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA1L-WEv6NSWgf8DNFrRy2ncfqJWRrnjzQ",
+        authDomain: "seg3102-lab5-ff7fd.firebaseapp.com",
+        projectId: "seg3102-lab5-ff7fd",
+        storageBucket: "seg3102-lab5-ff7fd.appspot.com",
+        messagingSenderId: "544221672893",
+        appId: "1:544221672893:web:ae63119f11eac0b4e1ced4"
+      }
+};
